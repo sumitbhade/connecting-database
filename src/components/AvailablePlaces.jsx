e8ed9853a20d69323e0c78f41a9544def6e43e0f
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-16 23:14:00 +0530

    Transforming fetched data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { use } from "react";
 import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -33,12 +34,22 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error("Failed to fetch new places");
         }
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "Could not fetch" });
-      }
 
-      setIsFetching(false);
+        setIsFetching(false);
+      }
     }
     fetchPlaces();
   }, []);
