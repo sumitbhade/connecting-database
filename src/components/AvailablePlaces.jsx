e0e3b7d560fc28bf57a0958afcb710e4285b9be2
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-17 12:16:13 +0530

    Improving Code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 import ErrorPage from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -28,16 +29,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch new places");
-        }
-
+        const places = await fetchAvailablePlaces();
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
